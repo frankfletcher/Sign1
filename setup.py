0f1f831c62c87a35231e7a17f18064cf84ae29ad
Frank Fletcher <cogsci2@gmail.com>
2020-09-14 12:21:07 -0700

    Initial commit - basic project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A quick test of FastAI on images to classify a small subset of American Sign Language.',
+    author='Frank Fletcher',
+    license='',
+)
