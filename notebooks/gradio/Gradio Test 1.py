9305aca25fcf4c2f600160d8312e287154c4b577
Frank Fletcher <cogsci2@gmail.com>
2021-03-04 01:31:57 -0800

    cleaned up the gradio deployment

--- notebooks/gradio/Gradio Test 1.py
+++ notebooks/gradio/Gradio Test 1.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# import sys
-# sys.path.append('../utils')
-
-
 import warnings
 warnings.filterwarnings('ignore')  # or 'once'  
-
-
 
 import pyforest
 import gradio as gr
@@ -23,23 +17,17 @@ from nn_utils_eff import *
 from augmentation import *
 
 
-
-
 RUN_NAME = '20210303-1849 - arch=tf_efficientnet_b4_ns - samples=1100 frozen=1 epochs=15 bs=48 res=380 _data=combined2'
 
 learn_inf = load_learner(f'{RUN_NAME}.pkl', cpu=True)
 remove_cbs(learn_inf)
-print('Model Loaded.')
-
+# print('Model Loaded.')
 
 learn_inf.remove_cbs([NonNativeMixedPrecision, ModelToHalf])
-learn_inf.cbs
-
 
 labels = [char for char in ascii_uppercase]
 labels.remove('J')
 labels.remove('Z')
-
 
 def get_sign(img):
     pred,pred_idx,probs = learn_inf.predict(img)
