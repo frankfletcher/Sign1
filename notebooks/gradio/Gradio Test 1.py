693712ad6162bf5a36eb4b6a07a53c3b93c3190d
Frank Fletcher <cogsci2@gmail.com>
2021-03-04 00:36:20 -0800

    added EfficientNet model, pickle and gradio implementation

---
+++ notebooks/gradio/Gradio Test 1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+sys.path.append('../utils')
+
+
+import warnings
+warnings.filterwarnings('ignore')  # or 'once'  
+
+
+
+import pyforest
+import gradio as gr
+
+from datetime import datetime
+from fastbook import *
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+import fastai
+import gc
+from nn_utils_eff import *
+from augmentation import *
+
+
+
+RUN_NAME = '20210303-1849 - arch=tf_efficientnet_b4_ns - samples=1100 frozen=1 epochs=15 bs=48 res=380 _data=combined2'
+
+learn_inf = load_learner(f'../../models/{RUN_NAME}.pkl', cpu=True)
+remove_cbs(learn_inf)
+print('Model Loaded.')
+
+
+learn_inf.remove_cbs([NonNativeMixedPrecision, ModelToHalf])
+learn_inf.cbs
+
+
+labels = [char for char in ascii_uppercase]
+labels.remove('J')
+labels.remove('Z')
+
+
+def get_sign(img):
+    pred,pred_idx,probs = learn_inf.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+ 
+image = gr.inputs.Image(shape=(380,380))
+label = gr.outputs.Label(num_top_classes=3)
+iface = gr.Interface(fn=get_sign, inputs=image, outputs=label)
+iface.launch(share=False)
+
+
+
+
